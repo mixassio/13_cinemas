32009c9dc7d413e3c6e74c4e2e7e73e4129b78f3
mixassio <mixail.bondarev@gmail.com>
2017-06-24 13:53:08 +0300

    check mistake

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # Cinemas
 
-This script displays a list of 10 films with high ratings, which are now in theaters of Moscow
-Just run the script:
+The script displays the most popular films, currently going, sorted by rating.
+Data from afisha.ru and kinopoisk.ru are used
+
+To run the script requires an interpreter python 3
+
+Output data:
+title - name of film
+year - year of issue
+count_cinema - The number of cinemas in which the film is currently running
+rating_kp - Rating of the film from kinopoisk.ru
+
 ```#!bash
 
-$ python cinemas.py
+$ python3 cinemas.py
+
 {'title': 'Тачки-3', 'year': '2017', 'count_cinema': 160, 'rating_kp': 7.099}
 {'title': 'Мумия', 'year': '2017', 'count_cinema': 157, 'rating_kp': 5.854}
 {'title': 'Чудо-женщина', 'year': '2017', 'count_cinema': 145, 'rating_kp': 7.034}
--- cinemas.py
+++ cinemas.py
@@ -3,13 +3,15 @@ import requests
 import bs4
 
 
+COUNT_FILM_OUT = 10
+
 def fetch_afisha_list():
     films_html = requests.get('https://www.afisha.ru/msk/schedule_cinema/')
     soup = bs4.BeautifulSoup(films_html.content, "lxml")
     list_films = []
-    for i in soup('h3', {'class': 'usetags'}):
-        name_film = i.a.string
-        id_film_afisha = re.findall('https://www.afisha.ru/movie/(\d*)', i.a['href'])[0]
+    for film in soup('h3', {'class': 'usetags'}):
+        name_film = film.a.string
+        id_film_afisha = re.findall('https://www.afisha.ru/movie/(\d*)', film.a['href'])[0]
         year_film, count_cinema = get_count_schedule_film(id_film_afisha)
         list_films.append({'title': name_film, 'year': year_film, 'count_cinema': count_cinema})
     return list_films
@@ -29,20 +31,20 @@ def fetch_idfilm_kp(title_film, year_film):
     response = requests.get(url)
     id_film_kp = re.findall('https://www.kinopoisk.ru/film/(\d*)', response.history[-1].url)
     if id_film_kp:
-        return id_film_kp[0]
+        return id_film_kp[0] #The situation when kinopoisk.ru is translated immediately to the page of the film (an unambiguous coincidence)
     else:
         soup = bs4.BeautifulSoup(response.content, "lxml")
         film = soup.findAll('p', {'class': 'pic'})
         if film:
             id_film_kp = re.findall('data-id="(\d*)"', str(film[0]))
-            return id_film_kp[0]
+            return id_film_kp[0] #The situation when film search is translated into a page with several similar films (multiple coincidence)
         else:
-            return None
+            return None #Movie not found
 
 
 def get_rating_kp(id_film_kp):
     if id_film_kp is None:
-        return '0', '0' #Not all movies are on kinopoisk
+        return None, None #Not all movies are on kinopoisk
     url = 'http://www.kinopoisk.ru/rating/{}.xml'.format(id_film_kp)
     raw_xml = requests.get(url).content
     soup = bs4.BeautifulSoup(raw_xml, "lxml")
@@ -51,22 +53,22 @@ def get_rating_kp(id_film_kp):
     return rating_kp, count_wiewer
 
 
-def get_list_10():
+def get_list():
     list_films = fetch_afisha_list()
     set_delete = []
     for id, film in enumerate(list_films):
         id_film_kp = fetch_idfilm_kp(film['title'], film['year'])
         rating_kp, count_wiewer = get_rating_kp(id_film_kp)
-        film['rating_kp'] = float(rating_kp)
-        if float(rating_kp) == 0.0:
+        film['rating_kp'] = rating_kp
+        if rating_kp is None or float(rating_kp) == 0.0:
             set_delete.append(id)
-    set_delete.reverse()
+    set_delete.reverse() #We use for work pop(). Do not change the list of movies in some places
     for id in set_delete:
         list_films.pop(id)
-    return list_films[:10]
+    return list_films
 
 
 if __name__ == '__main__':
-    list_films = get_list_10()
-    for i in sorted(list_films, reverse=True, key=lambda k: (k['count_cinema'], k['rating_kp'])):
-        print(i)
+    list_films = get_list()
+    for film in sorted(list_films, reverse=True, key=lambda k: (k['count_cinema'], k['rating_kp']))[:COUNT_FILM_OUT]:
+        print(film)
